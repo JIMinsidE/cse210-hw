fcb4cac764bab382b067bb296becf1f098219e52
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-06 07:30:24 +0200

    tsk3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
 class Program
+
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 100);
+    int guess = 0, count = 0;
+    
+    while (guess != number)
+    {
+      Console.WriteLine("Guess the number");
+      string tguess = Console.ReadLine();
+      guess = int.Parse(tguess);
+      if (guess > number){
+        Console.WriteLine("Lower");
+      }
+      else if (guess < number){
+        Console.WriteLine("Higher");
+      }
+      count +=1;
     }
+    Console.WriteLine("You guessed it!");
+    Console.WriteLine($"Number of guesses: {count}");
+  }
+    
 }
