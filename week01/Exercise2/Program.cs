478186ef5c42a8cf7713c309aa30db75fef0f509
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-06 07:15:50 +0200

    num2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,102 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your mark: ");
+        string mark = Console.ReadLine();
+        int number = int.Parse(mark);
+        string letter = "";
+        
+        if (number < 0 || number > 100)
+            {
+                Console.WriteLine("Invalid input. Please enter a grade between 0 and 100.");
+                return; 
+            }
+
+            
+
+            if (number >= 90)
+            {
+                letter = "A";
+            }
+            else if (number >= 80)
+            {
+                letter = "B";
+            }
+            else if (number >= 70)
+            {
+                letter = "C";
+            }
+            else if (number >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+            
+        int lastDigit = number % 10; 
+
+            if (letter != "A" && lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            else if (letter != "F" && lastDigit <= 3)
+            {
+                letter += "-";
+            }
+        Console.WriteLine($"Your letter grade is:{letter}");
+    }
+}using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your mark: ");
+        string mark = Console.ReadLine();
+        int number = int.Parse(mark);
+        string letter = "";
+        
+        if (number < 0 || number > 100)
+            {
+                Console.WriteLine("Invalid input. Please enter a grade between 0 and 100.");
+                return; 
+            }
+
+            
+
+            if (number >= 90)
+            {
+                letter = "A";
+            }
+            else if (number >= 80)
+            {
+                letter = "B";
+            }
+            else if (number >= 70)
+            {
+                letter = "C";
+            }
+            else if (number >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+            
+        int lastDigit = number % 10; 
+
+            if (letter != "A" && lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            else if (letter != "F" && lastDigit <= 3)
+            {
+                letter += "-";
+            }
+        Console.WriteLine($"Your letter grade is:{letter}");
     }
 }
