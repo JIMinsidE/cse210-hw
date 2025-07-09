58a6a9b6a41a91cf7b760617d812867d0969fb2f
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-09 23:00:06 +0200

    just task №4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+using System.Collections.Generic;
+ 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers;
+        numbers = new List<int>();
+        
+        string exit_Number = "";
+        while (exit_Number != "0")
+        {
+            Console.WriteLine("Enter number: ");
+            exit_Number = Console.Readline();
+            int input_Number = parseInt(exit_Number);
+            numbers.Add(input_Number);
+            
+        }
+
     }
 }
