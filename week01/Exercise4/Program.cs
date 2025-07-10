431e8ef25a6584d2e6a89e26876bc11ca29971b4
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-10 18:42:32 +0200

    final version.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
- 
+using System.Linq;
+
 class Program
 {
     static void Main(string[] args)
@@ -10,15 +11,37 @@ class Program
         List<int> numbers;
         numbers = new List<int>();
         
-        string exit_Number = "";
+        string exit_Number = "1";
         while (exit_Number != "0")
         {
             Console.WriteLine("Enter number: ");
-            exit_Number = Console.Readline();
-            int input_Number = parseInt(exit_Number);
+            exit_Number = Console.ReadLine();
+            int input_Number = int.Parse(exit_Number);
+            if (input_Number != 0){
             numbers.Add(input_Number);
-            
+            }
         }
 
+        numbers.Sort();
+        int numbers_sum = 0;
+        int min_number = 1;
+
+        foreach (int number in numbers)
+        {
+            numbers_sum += number;
+
+            if (number > 0 && number <= min_number){
+                min_number = number;
+            } 
+        }
+        Console.WriteLine($"The sum is: {numbers_sum}");
+        Console.WriteLine($"The average is: {numbers_sum/numbers.Count}");
+        Console.WriteLine($"The larges number is: {numbers.ElementAt(numbers.Count-1)}");
+        Console.WriteLine($"The smallest positive number is: {min_number}");
+        Console.WriteLine("The sorted list is:");
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
