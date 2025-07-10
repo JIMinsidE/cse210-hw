a1b2c503c574ee3d46f76a3055fc1773a303dedc
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-10 20:06:41 +0200

    Num5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine($"Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine($"Please enter your favorite number:");
+        string input_number = Console.ReadLine();
+        int number = int.Parse(input_number);
+        return number;
+    }
+
+    static double SquareNumber(int number)
+    {
+        double square = Math.Pow(number, 2);
+        return square;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {SquareNumber(number)}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(name, number);
     }
 }
