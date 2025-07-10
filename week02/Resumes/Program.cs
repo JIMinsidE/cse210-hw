fcc9f81f1223bff628cd1b9bf1ce3a9734b0f77d
Aleksandr_Shimanovich <115615136+JIMinsidE@users.noreply.github.com>
2025-07-11 00:19:28 +0200

    e

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
+public class Resume()
 {
-    static void Main(string[] args)
+    public string _name;
+    public List<Job> _jobs = new List<Job>() {};
+
+    public void Display()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
     }
 }
