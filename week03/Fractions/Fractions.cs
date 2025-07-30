00d57b4b3b03eb2483152a93f7bac07e2357d098
JIMinsidE <realbreadinside@gmail.com>
2025-07-30 09:13:59 +0200

    fractions

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+public class Fractions
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fractions()
+    {
+        _numerator = 1;
+        _denominator = 1;
+        
+    }
+    public Fractions(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+    public Fractions(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+    public string GetfractionString()
+    {
+        string frac_string = $"{_numerator}/{_denominator}";
+        return frac_string;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions fraction1 = new Fractions();
+        Console.WriteLine("Default Fraction: " + fraction1.GetfractionString());
+        Console.WriteLine("Decimal Value: " + fraction1.GetDecimalValue());
+
+        Fractions fraction2 = new Fractions(1);
+        Console.WriteLine("Whole Number Fraction: " + fraction2.GetfractionString());
+        Console.WriteLine("Decimal Value: " + fraction2.GetDecimalValue());
+
+        Fractions fraction3 = new Fractions(2, 5);
+        Console.WriteLine("Custom Fraction: " + fraction3.GetfractionString());
+        Console.WriteLine("Decimal Value: " + fraction3.GetDecimalValue());
+
+        Fractions fraction4 = new Fractions(1, 3);
+        Console.WriteLine("Another Custom Fraction: " + fraction4.GetfractionString());
+        Console.WriteLine("Decimal Value: " + fraction4.GetDecimalValue());
     }
 }
