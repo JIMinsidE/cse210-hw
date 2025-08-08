77754b35e6f59e26419e986bfa19bf967aa9d984
JIMinsidE <realbreadinside@gmail.com>
2025-08-08 22:15:18 +0200

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create a MathAssignment instance
+        MathAssignment mathAssignment = new MathAssignment("John Atkinsonn", "Algebra", "Section 5.2", "Problems 1-10");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Create a WritingAssignment instances
+        WritingAssignment writingAssignment = new WritingAssignment("Jane Smith", "Literature", "The Great Gatsby Essay");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
