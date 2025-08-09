cb51035dba0ce3c35fdbaface2a8ada8c777367b
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 10:47:23 +0200

    BASE

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -6,7 +6,19 @@ class ListingActivity : Activity
     private int _count;
     private List<string> _prompts;
 
-    public ListingActivity() : base
+    public ListingActivity() : base(
+        "Listing Activity",
+        "This activity will help you reflect on the good things in your life " +
+        "by having you list as many things as you can in a certain area.")
+    {
+        _prompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
+        };
     }
 
     private string GetRandomPrompt()
