204789847384f500ecb6c74c613c6a82508d5a13
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 10:48:58 +0200

    bsefor breathing

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,4 +1,16 @@
-public class BreathingActivity : Activity
-{
+using System;
 
+class BreathingActivity : Activity
+{
+    public BreathingActivity() : base(
+        "Breathing Activity",
+        "This activity will help you relax by walking you through breathing in and out slowly. " +
+        "Clear your mind and focus on your breathing.")
+    {
+    }
+
+    public override void Run()
+    {
+
+    }
 }
