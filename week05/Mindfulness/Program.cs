0d69f0fdb863129d22fa764ddf95dd97cac23c7a
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 09:48:42 +0200

    main program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,23 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello user! Welcome to the Mindfulness Program.\nToday we will focus on mindfulness activities to help you relax and reflect.");
-        Console.WriteLine("Please choose an activity to start:\n1. Breathing Activity\n2. Listing Activity\n3. Reflection Activity");
-        string choice = Console.ReadLine();
-        switch (choice)
+        bool wantToQuit = false;
+        while (wantToQuit == false)
         {
-            case "1":
-                
-                break;
-            case "2":
-                
-                break;
-            case "3":
-                
-                break;
-            default:
-                Console.WriteLine("Invalid choice. Please try again.");
-                break;
+            Console.WriteLine("Hello user! Welcome to the Mindfulness Program.\nToday we will focus on mindfulness activities to help you relax and reflect.");
+            Console.WriteLine("Please choose an activity to start:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Listing Activity");
+            Console.WriteLine("3. Reflection Activity4. Exit");
+
+            Console.Write("Enter the number of your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity.Run();
+                    break;
+
+                case "2":
+                    ListingActivity.Run();
+                    break;
+
+                case "3":
+                    ReflectionActivity.Run();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Thank you for launching this program. We hope to it helped you. Goodbye!");
+                    wantToQuit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
         }
     }
 }
