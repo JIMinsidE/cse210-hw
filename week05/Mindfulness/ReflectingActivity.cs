a1d09b70f33ac5997d04f9e81f8940afb0f67a39
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 10:46:56 +0200

    getlist from user

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,4 +1,37 @@
-public class ReflectionActivity : Activity
-{
+using System;
+using System.Collections.Generic;
 
+class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity() : base
+    }
+
+    private string GetRandomPrompt()
+    {
+
+    }
+
+    private List<string> GetListFromUser()
+    {
+        List<string> items = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string? item = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(item))
+            {
+                items.Add(item);
+            }
+        }
+        return items;
+    }
+
+    public override void Run()
+    {
+    
+    }
 }
