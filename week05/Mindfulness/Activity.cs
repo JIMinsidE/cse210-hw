ae631bbb7a14ca257b746af02bc2a9225685ca09
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 09:48:04 +0200

    long work

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,17 +1,73 @@
 public class Activity
 {
-    protected string _finishingMessage = "Thank you for participating in this mindfulness activity. We hope you feel more relaxed and centered.";
-    private string Loading = "Loading...";
-    public Activity()
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+    public Activity(string name, string description)
     {
-        
+        _name = name;
+        _description = description;
     }
-    public string getLoading()
+    
+public void DisplayStartingMessage()
     {
+        Console.WriteLine($"\nWelcome to the {_name}.");
+        Console.WriteLine(_description);
+        Console.Write("Consider to enter duration in seconds: ");
 
+        _duration = int.Parse(Console.ReadLine());
+        switch (_duration)
+        {
+            case 0:
+                Console.WriteLine("Duration cannot be zero. Setting to default of 31 seconds.");
+                _duration = 31;
+                break;
+            case < 1:
+                Console.WriteLine("Duration must be at least 1 second. Setting to default of 31 seconds.");
+                _duration = 31;
+                break;
+            case > 60:
+                Console.WriteLine("Duration is too long. Setting to maximum of 60 seconds.");
+                _duration = 60;
+                break;
+            default:
+                _duration = 30;
+                break;
+        }
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
     }
-    public string getFinishingMessage()
+
+    public void DisplayEndingMessage()
     {
-        return _finishingMessage;
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(2);
+        Console.WriteLine($"You have completed {_duration} seconds of {_name}.");
+        ShowSpinner(2);
     }
+
+    public void ShowSpinner(int seconds)
+    {
+        string[] sequence = { "|", "/", "-", "\\" };
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(sequence[i % sequence.Length]);
+            Thread.Sleep(200);
+            Console.Write("\b \b");
+            i++;
+        }
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+    
 }
