8c72b253d10a2546a0c4367c14d7760b48753221
JIMinsidE <realbreadinside@gmail.com>
2025-08-09 07:49:42 +0200

    Activity

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,4 +1,17 @@
 public class Activity
 {
-    // Base class for activities
+    protected string _finishingMessage = "Thank you for participating in this mindfulness activity. We hope you feel more relaxed and centered.";
+    private string Loading = "Loading...";
+    public Activity()
+    {
+        
+    }
+    public string getLoading()
+    {
+
+    }
+    public string getFinishingMessage()
+    {
+        return _finishingMessage;
+    }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Hello user! Welcome to the Mindfulness Program.\nToday we will focus on mindfulness activities to help you relax and reflect.");
+        Console.WriteLine("Please choose an activity to start:\n1. Breathing Activity\n2. Listing Activity\n3. Reflection Activity");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                
+                break;
+            case "2":
+                
+                break;
+            case "3":
+                
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
     }
 }
