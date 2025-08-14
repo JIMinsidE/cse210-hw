b5cd6e09018dede73636b073a6ec03d559008e83
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 06:38:32 +0200

    etgoal

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,19 +1,19 @@
 public abstract class Goal
 {
-    public string _shortname;
-    public string _description;
-    private int _points;
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
 
-    public Goal(string shortname, string description, int points)
+    public Goal(string name, string description, int points)
     {
-        _shortname = shortname;
+        _shortName = name;
         _description = description;
         _points = points;
     }
 
     public abstract int RecordEvent();
     public abstract bool IsComplete();
-    public abstract string GetStringRepresentasion();
+    public abstract string GetStringRepresentation();
 
     public virtual string GetDetails()
     {
--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -1,17 +1,19 @@
 public class SimpleGoal : Goal
 {
+    private bool _isComplete;
+
     public SimpleGoal(string name, string description, int points)
         : base(name, description, points)
     {
         _isComplete = false;
     }
 
-    public override void GetStringRepresentation()
+    public override string GetStringRepresentation()
     {
         return $"SimpleGoal|{_shortName}|{_description}|{_points}|{_isComplete}";
     }
 
-    public override void RecordEvent()
+    public override int RecordEvent()
     {
         if (!_isComplete)
         {
