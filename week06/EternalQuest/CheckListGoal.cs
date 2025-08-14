cc02e31640207d0121cff4f0a6b3e0acfacc87a9
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 06:42:15 +0200

    underconstruction

---
+++ week06/EternalQuest/CheckListGoal.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class CheckListGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public CheckListGoal(string name, string description, int points, int target, int bonus)
+        : base(name, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+    
+}
