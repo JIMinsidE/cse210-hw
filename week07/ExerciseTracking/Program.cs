601984e6de2951131f670427c1189ef030d8be34
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 08:11:15 +0200

    mainend

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,12 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Activity> activities = new List<Activity>
-        {
-            new Running(new DateTime(2022, 11, 3), 30, 3.0),
-            new Cycling(new DateTime(2022, 11, 3), 30, 12.0),
-            new Swimming(new DateTime(2022, 11, 3), 30, 20)
-        };
+        List<Activity> activities = new List<Activity>();
+
+        Running run1 = new Running(new DateTime(2022, 11, 3), 30, 3.0, 6.0); // Added speedInMph argument
+        activities.Add(run1);
+
+        Cycling cycl1 = new Cycling(new DateTime(2022, 11, 3), 30, 12.0);
+        activities.Add(cycl1);
+
+        Swimming swim1 = new Swimming(new DateTime(2022, 11, 3), 30, 20);
+        activities.Add(swim1);
+        
 
         foreach (Activity activity in activities)
         {
