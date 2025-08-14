cedece91efb9ddbb5a495e59d12f686a544cbabd
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 07:57:25 +0200

    summ

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -3,13 +3,24 @@ abstract class Activity
     private DateTime _date;
     private double _lengthInMinutes;
 
+    public double LengthMinutes => _lengthInMinutes;
+    public DateTime Date => _date;
+
     public Activity(DateTime date, double lengthInMinutes)
     {
         _date = date;
         _lengthInMinutes = lengthInMinutes;
     }
 
-        public abstract double GetDistance(); // in miles
+    public abstract double GetDistance(); // in miles
     public abstract double GetSpeed();    // mph
     public abstract double GetPace();     // min per mile
+
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {GetType().Name} ({LengthMinutes} min) - " +
+               $"Distance: {GetDistance():0.0} miles, " +
+               $"Speed: {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
 }
