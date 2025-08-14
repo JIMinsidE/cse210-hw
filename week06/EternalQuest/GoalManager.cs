c21a745f2d15b38783664b667e5a3eab524b5c41
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 06:55:22 +0200

    podstawa

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -1,4 +1,39 @@
 public class GoalManager
 {
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+    }
+
+    public void Start()
+    {
+
+    }
+
+    private void CreateGoal()
+    {
+
+    }
+
+    private void ListGoalDetails()
+    {
+
+    }
+
+    private void RecordEventMenu()
+    {
+
+    }
+
+    private void SaveGoals()
+    {
+
+    }
+    private void LoadGoals()
+    {
+
+    }
     
 }
