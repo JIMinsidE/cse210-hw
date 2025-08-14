ac739af06fad148e8f1d720389fcacfe8c99fcb4
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 08:06:54 +0200

    swimend

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,4 +1,24 @@
 class Swimming : Activity
 {
-    
+    private int _laps;
+
+    public Swimming(DateTime date, double lengthMinutes, int laps)
+        : base(date, lengthMinutes)
+    {
+        _laps = laps;
+    }
+    public override double GetDistance()
+    {
+        double kilometers = _laps * 50 / 1000.0;
+        return kilometers * 0.62; // convert to miles
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / LengthMinutes) * 60;
+    }
+    public override double GetPace()
+    {
+        return LengthMinutes / GetDistance();
+    }
 }
