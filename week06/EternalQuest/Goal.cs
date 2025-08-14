d7f67bb766f5dd8f1fa0053552a0aac6f162c9f1
JIMinsidE <realbreadinside@gmail.com>
2025-08-14 06:13:47 +0200

    le starte

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,8 +1,18 @@
-public class Goal
+public abstract class Goal
 {
-    public string Name;
-    public string Description;
-    private int Points;
+    public string _name;
+    public string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract int RecordEvent();
     public abstract bool IsComplete();
 
+    
 }
